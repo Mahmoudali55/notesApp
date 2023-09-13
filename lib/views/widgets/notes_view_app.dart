3604b5f43d47322cda59a5a43a27e3dd6df4b4c5
mahmoudali15 <130462575+mahmoudali15@users.noreply.github.com>
2023-09-13 16:03:02 +0300

    custom_note_item

--- lib/views/widgets/notes_view_app.dart
+++ lib/views/widgets/notes_view_app.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/views/widgets/customAppbar.dart';
+import 'package:notes_app/views/widgets/custom_noteitem.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -11,7 +13,10 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
       child: Column(
-        children: [CustomAppBar()],
+        children: [
+          CustomAppBar(),
+          NoteItem(),
+        ],
       ),
     );
   }
